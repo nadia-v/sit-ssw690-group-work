7890aa5a7b7cdbcf4966f312991753d29d6413a2
Nadia Vedeneyeva <nadiaveden@gmail.com>
2019-03-02 06:59:09 -0500

    Second quack

--- quack/quack/urls.py
+++ quack/quack/urls.py
@@ -22,7 +22,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homes.views.homepage, name='home'),
-    
-    
+    path('homes/<int:service_id>', homes.views.section, name='section')
+       
 ] 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
