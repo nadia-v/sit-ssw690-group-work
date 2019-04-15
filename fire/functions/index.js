1cedcb142b6e2bb5df7a44bff1cbbc8f584d117e
Nadia Vedeneyeva <nadiaveden@gmail.com>
2019-04-15 13:16:33 -0400

    trying connect to firebase

--- fire/functions/index.js
+++ fire/functions/index.js
@@ -1,12 +1,21 @@
-const functions = require('firebase-functions');
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+const functions = require('firebase-functions');
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    console.log("Hello")
-    response.send("Nadik Hello from Firebase!");
+exports.bigben = functions.https.onRequest((req, res) => {
+  const hours = (new Date().getHours() % 12) + 1  // London is UTC + 1hr;
+  res.status(200).send(`<!doctype html>
+    <head>
+      <title>Time</title>
+    </head>
+    <body>
+      ${'BONG '.repeat(hours)}
+    </body>
+  </html>`);
 });
-
-
-
