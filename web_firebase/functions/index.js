311f5d6ae04d525175247833ea63174b29685e3c
Raymond Yin <raymondxiwei@gmail.com>
2019-04-15 13:16:21 -0400

    added new pages and redirect from login to dashboard.

--- web_firebase/functions/index.js
+++ web_firebase/functions/index.js
@@ -1,5 +1,9 @@
-// Cloud Functions: https://firebase.google.com/docs/functions/write-firebase-functions
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+const firebase = require('firebase/app');
+const firebase_auth = require('firebase/auth');
 
+// Cloud Functions: https://firebase.google.com/docs/functions/write-firebase-functions
 const functions = require('firebase-functions');
 const path = require("path");
 const express = require('express');
@@ -15,14 +19,38 @@ app.engine("handlebars", hbs({
 app.set("view engine", "handlebars");
 
 
+let firebaseConfig = {
+    apiKey: "AIzaSyCC78yv8F0WtVctcQJrleAwyK3J9CLLf-I",
+    authDomain: "raymond-firebase-exer.firebaseapp.com",
+    databaseURL: "https://raymond-firebase-exer.firebaseio.com",
+    projectId: "raymond-firebase-exer",
+    storageBucket: "raymond-firebase-exer.appspot.com",
+    messagingSenderId: "1038252815830"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
 // Route and API functions below
 
-app.get('/', (request, response) => {
-    response.render('login', {
-        item: {
-            name: "raymond"
-        }
-    })
+app.get('/app', (request, response) => {
+    // or use 
+    // let user = firebase.auth.Auth.user;
+    let user = firebase.auth().currentUser;
+    if (user) {
+        console.log("user signed in:");
+        console.log(user);
+        response.render('dashboard', {});
+    } else {
+        console.log("user is probably null:");
+        console.log(user);
+        response.render('user_page', {
+            item: {
+                name: "raymond"
+            }
+        });
+    }
 })
 
 exports.app = functions.https.onRequest(app);
