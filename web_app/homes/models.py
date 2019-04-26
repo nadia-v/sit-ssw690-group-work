fa9e7489f9cc82fdd856e921885904e7759ae39e
Raymond Yin <raymondxiwei@gmail.com>
2019-04-26 16:45:55 -0400

    fixed untracked files and restructured project folders

--- web_app/homes/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Home(models.Model):
-    image = models.ImageField(upload_to='images/')
-    service = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.service
