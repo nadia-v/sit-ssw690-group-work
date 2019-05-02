c7399bbeaa8fd8a8490f982b594f5697350841c1
Raymond Yin <raymondxiwei@gmail.com>
2019-05-02 17:30:50 -0400

    fixed display for post card pictures.

--- web_firebase/public/js/dashboard.js
+++ web_firebase/public/js/dashboard.js
@@ -7,9 +7,43 @@ function logout() {
     });
 };
 
+
+function timeSince(date) {
+
+    var seconds = Math.floor((new Date() - date) / 1000);
+
+    var interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) {
+        return interval + " years";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+        return interval + " months";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+        return interval + " days";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+        return interval + " hours";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+        return interval + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
+}
+var aDay = 24 * 60 * 60 * 1000
+console.log(timeSince(new Date(Date.now() - aDay)));
+console.log(timeSince(new Date(Date.now() - aDay * 2)));
+
 function convertTimeFormat(unixTimestamp) {
     // helper function to convert any unix timestamps into human readable format
-    var date = new Date(unixTimestamp * 1000); // Javascript stores dates as milliseconds
+    var date = new Date(unixTimestamp); // Javascript stores dates as milliseconds
+    // console.log("time:");
+    console.log(date.toDateString());
     // // Hours part from the timestamp
     // var hours = date.getHours();
     // // Minutes part from the timestamp
@@ -18,24 +52,29 @@ function convertTimeFormat(unixTimestamp) {
     // var seconds = "0" + date.getSeconds();
     // // Will display time in 10:30:23 format
     // var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-    return date.toDateString();
+    console.log(Date.now());
+    console.log(unixTimestamp);
+    return timeSince(unixTimestamp);
 }
 
 function generatePostCard(dbEntry) {
     if (!dbEntry) {
         return null;
     } else {
-        var displayTime = convertTimeFormat(dbEntry.data.adDate);
-        var userID = dbEntry.data.idUser;
+        var displayTime = convertTimeFormat(dbEntry.data().adDate);
+        var userID = dbEntry.data().idUser;
+        var url = dbEntry.data().imageOne;
+
         // db.collection("user").
         var parser = new DOMParser();
         var domString =
             '<div class="row"><div class="col-lg-12 mb-4">' +
             '<div class="card shadow mb-4">' +
-            '<div class="card-header py-3">' + '<h6 class="m-0 font-weight-bold text-primary">' + dbEntry.data().adTitle + '</h6>' + '</div>' +
+            '<div class="card-header py-3">' + '<h6 class="m-0 font-weight-bold text-primary">' + dbEntry.data().adTitle + '</h6>' + '<h6 class="m-0 text-right">' + displayTime + ' ago</h6>' +
+            '</div>' +
             '<div class="card-body">' +
             '<div class="text-center">' +
-            '<img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;" src="img/undraw_posting_photo.svg" alt="">' +
+            '<img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;" src="' + url + '" alt="">' +
             '</div>' +
             '<p>' + dbEntry.data().adDescription + '</p>' +
             '</div>' + '<div class="card-footer py-3">' +
@@ -57,9 +96,9 @@ function pullPosts(category, targetColumn) {
         var times = 0;
         snapshot.docs.forEach(function (doc) {
             var pageContent = document.getElementById(targetColumn);
-            console.log(doc.data());
+            // console.log(doc.data());
             var newElementToInsert = generatePostCard(doc).body.firstChild;
-            console.log(newElementToInsert);
+            // console.log(newElementToInsert);
 
             pageContent.appendChild(newElementToInsert);
             times++;
@@ -71,7 +110,7 @@ function initApp() {
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
             // User is signed in.
-            console.log(JSON.stringify(user));
+            // console.log(JSON.stringify(user));
             var currentUserFromFirebase = user; // save current user's information for other usage
             var userEmail = user.email;
             var userId = user.uid;
@@ -88,43 +127,6 @@ function initApp() {
             pullPosts("social_posts", "socialplace");
 
             var marketplace_posts = document.getElementById("marketplace_posts");
-
-
-            // db.collection("marketplace_posts").onSnapshot(function (querySnapshot) {
-
-            //     querySnapshot.forEach(function (doc) {
-
-            //         const storageRef = firebase.storage().refFromURL(doc.data().imageOne);
-            //         storageRef.getDownloadURL().then(function (url) {
-            //             // document.getElementById("marketplace-post-image").src = url;
-            //             marketplace_posts.innerHTML = " <div class=marketplace-posts><h1>Title: " + doc.data().adTitle +
-            //                 "</h1><h2>Category: " + doc.data().adCategory +
-            //                 "</h2><h2>Description: " + doc.data().adDescription +
-            //                 "</h2><h2>Status: " + doc.data().adStatus +
-            //                 "</h2><h2>Price: " + doc.data().adPrice +
-            //                 "</h2><h2>Posted on: " + doc.data().adDate + "</h2></div>"
-            //         })
-            //     })
-            // })
-
-            // db.collection("marketplace_posts")
-            //     .orderBy("editDate", "desc")
-            //     .limit(1).get().then(function (prevSnapshot) {
-
-            //         prevSnapshot.forEach(function (doc1) {
-
-            //             const storageRef = firebase.storage().refFromURL(doc1.data().imageOne);
-            //             storageRef.getDownloadURL().then(function (url) {
-            //                 // document.getElementById("marketplace-post-image1").src = url;
-            //                 marketplace_posts.innerHTML = " <div class=marketplace-posts1><h1>Title: " + doc1.data().adTitle +
-            //                     "</h1><h2>Category: " + doc1.data().adCategory +
-            //                     "</h2><h2>Description: " + doc1.data().adDescription +
-            //                     "</h2><h2>Status: " + doc1.data().adStatus +
-            //                     "</h2><h2>Price: " + doc1.data().adPrice +
-            //                     "</h2><h2>Posted on: " + doc1.data().adDate + "</h2></div>"
-            //             })
-            //         })
-            //     })
 
 
             //------------------------------------------------------------------------//
@@ -262,6 +264,7 @@ function initApp() {
             })
         } else {
             // User is signed out.
+            window.location = "/";
         }
 
     });
@@ -273,182 +276,5 @@ function initApp() {
 };
 
 window.onload = function () {
-
     initApp();
-
 };
-
-// var email_id = user.email;
-
-//             document.getElementById("user_para").innerHTML = "Welcome user " + email_id;
-
-
-// /**
-//  * Handles the sign in button press.
-//  */
-// function toggleSignIn() {
-//     if (firebase.auth().currentUser) {
-//         // [START signout]
-//         firebase.auth().signOut();
-//         // [END signout]
-//     } else {
-//         var email = document.getElementById('email').value;
-//         var password = document.getElementById('password').value;
-//         if (email.length < 4) {
-//             alert('Please enter an email address.');
-//             return;
-//         }
-//         if (password.length < 4) {
-//             alert('Please enter a password.');
-//             return;
-//         }
-//         // Sign in with email and pass.
-//         // [START authwithemail]
-//         firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-//             // Handle Errors here.
-//             var errorCode = error.code;
-//             var errorMessage = error.message;
-//             // [START_EXCLUDE]
-//             if (errorCode === 'auth/wrong-password') {
-//                 alert('Wrong password.');
-//             } else {
-//                 alert(errorMessage);
-//             }
-//             console.log(error);
-//             document.getElementById('quickstart-sign-in').disabled = false;
-//             // [END_EXCLUDE]
-//         });
-//         // [END authwithemail]
-//     }
-//     document.getElementById('quickstart-sign-in').disabled = true;
-//     console.log(user);
-// }
-
-// /**
-//  * Handles the sign up button press.
-//  */
-// function handleSignUp() {
-//     var email = document.getElementById('email').value;
-//     var password = document.getElementById('password').value;
-//     if (email.length < 4) {
-//         alert('Please enter an email address.');
-//         return;
-//     }
-//     if (password.length < 4) {
-//         alert('Please enter a password.');
-//         return;
-//     }
-//     // Sign in with email and pass.
-//     // [START createwithemail]
-//     firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
-//         // Handle Errors here.
-//         var errorCode = error.code;
-//         var errorMessage = error.message;
-//         // [START_EXCLUDE]
-//         if (errorCode == 'auth/weak-password') {
-//             alert('The password is too weak.');
-//         } else {
-//             alert(errorMessage);
-//         }
-//         console.log(error);
-//         // [END_EXCLUDE]
-//     });
-//     // [END createwithemail]
-// }
-
-// /**
-//  * Sends an email verification to the user.
-//  */
-// function sendEmailVerification() {
-//     // [START sendemailverification]
-//     firebase.auth().currentUser.sendEmailVerification().then(function () {
-//         // Email Verification sent!
-//         // [START_EXCLUDE]
-//         alert('Email Verification Sent!');
-//         // [END_EXCLUDE]
-//     });
-//     // [END sendemailverification]
-// }
-
-// function sendPasswordReset() {
-//     var email = document.getElementById('email').value;
-//     // [START sendpasswordemail]
-//     firebase.auth().sendPasswordResetEmail(email).then(function () {
-//         // Password Reset Email Sent!
-//         // [START_EXCLUDE]
-//         alert('Password Reset Email Sent!');
-//         // [END_EXCLUDE]
-//     }).catch(function (error) {
-//         // Handle Errors here.
-//         var errorCode = error.code;
-//         var errorMessage = error.message;
-//         // [START_EXCLUDE]
-//         if (errorCode == 'auth/invalid-email') {
-//             alert(errorMessage);
-//         } else if (errorCode == 'auth/user-not-found') {
-//             alert(errorMessage);
-//         }
-//         console.log(error);
-//         // [END_EXCLUDE]
-//     });
-//     // [END sendpasswordemail];
-// }
-
-// /**
-//  * initApp handles setting up UI event listeners and registering Firebase auth listeners:
-//  *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
-//  *    out, and that is where we update the UI.
-//  */
-// function initApp() {
-//     // Listening for auth state changes.
-//     // [START authstatelistener]
-//     console.log("reached here");
-//     var displayName = user.displayName;
-//     document.getElementById('username').textContent = displayName;
-//     firebase.auth().onAuthStateChanged(function (user) {
-//         // [START_EXCLUDE silent]
-//         document.getElementById('quickstart-verify-email').disabled = true;
-//         // [END_EXCLUDE]
-//         if (user) {
-//             // User is signed in.
-//             console.log("entered!");
-//             console.log(user);
-//             var displayName = user.displayName;
-//             var email = user.email;
-//             var emailVerified = user.emailVerified;
-//             var photoURL = user.photoURL;
-//             var isAnonymous = user.isAnonymous;
-//             var uid = user.uid;
-//             var providerData = user.providerData;
-//             // [START_EXCLUDE]
-//             document.getElementById('username').textContent = displayName;
-//             document.getElementById('quickstart-sign-in').textContent = 'Sign out';
-//             document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
-//             if (!emailVerified) {
-//                 document.getElementById('quickstart-verify-email').disabled = false;
-//             }
-//             // [END_EXCLUDE]
-//             window.location = '/main';
-//         } else {
-//             // User is signed out.
-//             // [START_EXCLUDE]
-//             document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
-//             document.getElementById('quickstart-sign-in').textContent = 'Sign in';
-//             document.getElementById('quickstart-account-details').textContent = 'null';
-//             // [END_EXCLUDE]
-//         }
-//         // [START_EXCLUDE silent]
-//         document.getElementById('quickstart-sign-in').disabled = false;
-//         // [END_EXCLUDE]
-//     });
-//     // [END authstatelistener]
-
-//     document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
-//     document.getElementById('quickstart-sign-up').addEventListener('click', handleSignUp, false);
-//     document.getElementById('quickstart-verify-email').addEventListener('click', sendEmailVerification, false);
-//     document.getElementById('quickstart-password-reset').addEventListener('click', sendPasswordReset, false);
-// }
-
-// window.onload = function () {
-//     initApp();
-// };
